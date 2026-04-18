4eda0edf6a1ccdbe6ae63e1438066501845c38be
Roberto Ayala <118330485+PavoAyala@users.noreply.github.com>
2026-04-18 16:13:28 -0600

    fix: corregir rutas y configuraciones en archivos de TypeScript y configuración de Next.js

--- apps/CRM/next.config.mjs
+++ apps/CRM/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	experimental: {
+		optimizePackageImports: ["firebase", "lucide-react"],
+	},
 	images: {
 		remotePatterns: [
 			{
